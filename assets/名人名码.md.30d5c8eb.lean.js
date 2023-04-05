6ddae9a06e781d4aa6af4798c9bb4e005445289d
azureplanet <121014103+azureplanet@users.noreply.github.com>
2023-04-05 17:11:27 +0800

    Add files via upload

---
+++ assets/名人名码.md.30d5c8eb.lean.js
@@ -0,0 +1,10 @@
+import{_ as a,o as l,c as p,d as o,a as s,e as n}from"./app.03c55a47.js";const g=JSON.parse('{"title":"名 人 名 码","description":"","frontmatter":{},"headers":[{"level":2,"title":"资讯","slug":"资讯","link":"#资讯","children":[]},{"level":2,"title":"更多","slug":"更多","link":"#更多","children":[]}],"relativePath":"名人名码.md"}'),e={name:"名人名码.md"},t=o("",5),c=s("div",{class:"tip custom-block"},[s("p",{class:"custom-block-title"},"宣传片"),s("iframe",{src:"//player.bilibili.com/player.html?aid=865860962&bvid=BV1D54y1M7g7&cid=1047540983&page=1",allowfullscreen:"allowfullscreen",width:"100%",height:"100%",scrolling:"no",frameborder:"0",sandbox:"allow-top-navigation allow-same-origin allow-forms allow-scripts"}," "),s("p",null,[n("【[像素工厂]：全新模组《蔚蓝行星》！不进来看看？】 "),s("a",{href:"https://www.bilibili.com/video/BV1D54y1M7g7/?share_source=copy_web&vd_source=2cfa775c9c40210fbc9c2c00e5decf44",target:"_blank",rel:"noreferrer"},"https://www.bilibili.com/video/BV1D54y1M7g7/?share_source=copy_web&vd_source=2cfa775c9c40210fbc9c2c00e5decf44")])],-1),r=s("h2",{id:"更多",tabindex:"-1"},[n("更多 "),s("a",{class:"header-anchor",href:"#更多","aria-hidden":"true"},"#")],-1),F=s("details",{class:"details custom-block"},[s("summary",null,"mod信息"),s("pre",null,[s("code",null,`名称:蔚蓝行星
+作者(们)(排名不分先后):zzc梓辰，屑猫贴贴，
+不太酸的小李子，
+渐变，子汐，萌新一只，
+维生素x，小柠檬，发生啥事，
+scarp，相织物回收，pardon，
+荔枝，嗷嗷怪，Ra2
+版本:100.6
+最小支持游戏版本:141
+`)])],-1),D=[t,c,r,F];function y(C,A,i,d,h,u){return l(),p("div",null,D)}const _=a(e,[["render",y]]);export{g as __pageData,_ as default};
